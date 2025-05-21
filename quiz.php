2b2d7db01794b63097b6a2dcb2e8464ad091cb3f
David Cano <davecano.244@gmail.com>
2025-05-21 12:33:24 -0400

    Fixed Questions in DB and began Quiz.php Session Verification

--- quiz.php
+++ quiz.php
@@ -1,14 +1,24 @@
 <?php // sql queries to actually grab questions and make the quiz
+session_start();
 
+if(!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
+    header("location.index.php");
+    exit;
+}
 
 require_once "config.php";
 $db = getDB();
 
+if(!isset($_GET['id'])){
+    die("Quiz not found or Invalsid Quiz ID");
+}
+
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Quiz</title>
+    <title></title>
 </head>
 <body>
 
--- quiz.sql
+++ quiz.sql
@@ -53,50 +53,50 @@ VALUES
 
 INSERT INTO questions
 VALUES
-    (1, "What is the capital of Mexico?", 1 ),
-    (2, "What is the capital of Lebanon?", 1),
-    (3, "What is the capital of Egypt?", 1),
-    (4, "What is the capital of Colombia?", 1),
-    (5, "What is the capital of Guatemala?", 1),
-    (6, "What is the capital of the Dominican Republic?", 1),
-    (7, "What is the capital of France?", 1),
-    (8, "What is the capital of Jamaica?", 1),
-    (9, "What is the capital of the Maldives?", 1),
-    (10, "What is the capital of Spain?",1),
-    (11, "What is the capital of the United States?", 1),
-    (12, "What is the capital of Italy?", 1),
-    (13, "What is the capital of Turkey?", 1),
-    (14, "What is the capital of South Korea?", 1),
-    (15, "What is the capital of Canada?", 1);
+    (1, 'What is the capital of Mexico?', 1 ),
+    (2, 'What is the capital of Lebanon?', 1),
+    (3, 'What is the capital of Egypt?', 1),
+    (4, 'What is the capital of Colombia?', 1),
+    (5, 'What is the capital of Guatemala?', 1),
+    (6, 'What is the capital of the Dominican Republic?', 1),
+    (7, 'What is the capital of France?', 1),
+    (8, 'What is the capital of Jamaica?', 1),
+    (9, 'What is the capital of the Maldives?', 1),
+    (10, 'What is the capital of Spain?',1),
+    (11, 'What is the capital of the United States?', 1),
+    (12, 'What is the capital of Italy?', 1),
+    (13, 'What is the capital of Turkey?', 1),
+    (14, 'What is the capital of South Korea?', 1),
+    (15, 'What is the capital of Canada?', 1);
 
 INSERT INTO answers
 VALUES
-    (1, "Mexico City", 1, true),
+    (1, 'Mexico City', 1, true),
 
-    (4, "Beirut", 2, true),
+    (4, 'Beirut', 2, true),
 
-    (7, "Cairo", 3, true),
+    (7, 'Cairo', 3, true),
 
-    (10, "Bogota", 4, true),
+    (10, 'Bogota', 4, true),
 
-    (13,"Guatemala City", 5, true),
+    (13,'Guatemala City', 5, true),
 
-    (16, "Santo Domingo", 6, true),
+    (16, 'Santo Domingo', 6, true),
 
-    (19,"Paris", 7 , true),
+    (19,'Paris', 7 , true),
 
-    (22, "Kingston", 8, true),
+    (22, 'Kingston', 8, true),
 
-    (25, "Malé", 9, true),
+    (25, 'Malé', 9, true),
 
-    (28, "Madrid", 10, true),
+    (28, 'Madrid', 10, true),
 
-    (31, "Washington DC", 11, true),
+    (31, 'Washington DC', 11, true),
 
-    (34, "Rome", 12, true),
+    (34, 'Rome', 12, true),
 
-    (37, "Istanbul", 13, true),
+    (37, 'Istanbul', 13, true),
 
-    (40, "Seoul", 14, true),
+    (40, 'Seoul', 14, true),
 
-    (43, "Ottawa", 15, true);
+    (43, 'Ottawa', 15, true);

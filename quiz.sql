52688becf96709024089591bd17fea564d5bb3d5
David Cano <davecano.244@gmail.com>
2025-05-21 20:13:28 -0400

    Added Quiz 2 Q/A

--- quiz.sql
+++ quiz.sql
@@ -47,7 +47,7 @@ CREATE TABLE scores (
 INSERT INTO quizzes
 VALUES
     (1, 'Capitals of the World'),
-    (2, 'Quiz 2 Temp Name');
+    (2, 'Pokemon Trivia');
 
 /* CREATE SAMPLE QUESTIONS*/
 
@@ -67,7 +67,23 @@ VALUES
     (12, 'What is the capital of Italy?', 1),
     (13, 'What is the capital of Turkey?', 1),
     (14, 'What is the capital of South Korea?', 1),
-    (15, 'What is the capital of Canada?', 1);
+    (15, 'What is the capital of Canada?', 1),
+
+    (16, 'What trainer owns Garchomp?',2),
+    (17, 'What region is the Pokemon Rayquaza from?',2),
+    (18, 'What was the first Pokemon Video Game to be featured on the Nintendo DS?', 2),
+    (19,'What is the Pokemon that had no weaknesses in the Pokemon Black and White Series?',2),
+    (20, 'What regions does the game Pokemon Stadium 2 Feature?', 2),
+    (21, 'What was the first Pokemon to be designed?', 2),
+    (22, 'What company owns the Pokemon franchise?', 2),
+    (23, 'What was the first Pokemon TCG set released?',2),
+    (24, 'What Pokemon is the ace of Lance in Pokemon HeartGold/SoulSilver?',2),
+    (25, 'What typing was introduced in Generation 6 (X/Y) ?', 2),
+    (26, 'What typing does Sceptile earn when Mega Evolving', 2),
+    (27,'What typing does Ampharaos earn when Mega Evolving', 2),
+    (28, 'What TM can be earned by trading in a RageCandyBar?', 2),
+    (29, 'What move can be learned in the Illex Forest', 2),
+    (30, 'Which Pokemon is the paradox version of Salamance?', 2);
 
 INSERT INTO answers
 VALUES
@@ -129,4 +145,65 @@ VALUES
 
     (43, 'Ottawa', 15, 1),
     (44, 'Toronto', 15, 0),
-    (45, 'Montreal', 15, 0);
+    (45, 'Montreal', 15, 0),
+
+    (46, 'Cynthia', 16,1),
+    (47, 'Blue', 16, 0),
+    (48, 'Falkner', 16,0),
+
+    (49, 'Hoenn', 17, 1),
+    (50, 'Johto', 17, 0),
+    (51, 'Sinnoh', 17, 0),
+
+    (52,'Pokemon Diamond and Pearl', 18, 1),
+    (53,'Pokemon Crystal', 18, 0),
+    (54, 'Pokemon Black and White 2', 18, 0),
+
+    (55,'Eelektross', 19, 1),
+    (56,'Reshriam', 19, 0),
+    (57, 'Kyurem', 19, 0),
+
+    (58, 'Kanto/Johto', 20, 1),
+    (59, 'Kanto/Hoenn', 20, 0),
+    (60, 'Johto/Hoenn', 20, 0),
+
+    (61, 'Rhydon', 21, 1),
+    (62, 'Pikachu', 21, 0),
+    (63, 'Mewtwo', 21, 0),
+
+    (64, 'Nintendo', 22, 1),
+    (65, 'Square Enix', 22, 0),
+    (66, 'Sega', 22,0),
+
+    (67, 'Base Set', 23, 1),
+    (68, 'Jungle', 23, 0),
+    (69, 'Fossil', 23, 0),
+
+    (70, 'Dragonite', 24, 1),
+    (71, 'Charizard', 24, 0),
+    (72, 'Salamance', 24, 0),
+
+    (73, 'Fairy', 25, 1),
+    (74, 'Air', 25,0),
+    (75, 'Dark', 25, 0),
+
+    (76, 'Dragon', 26, 1),
+    (77, 'Ground', 26, 0),
+    (78, 'Fire', 26, 0),
+
+    (79, 'Dragon', 27,1),
+    (80, 'Fire', 27, 0),
+    (81, 'Dark', 27,0),
+
+    (82, 'TM64 Explosion', 28, 1),
+    (83, 'TM12 Taunt', 28, 0),
+    (84, 'TM36 Sludge Bomb', 28, 0),
+
+    (85,'Headbutt', 29, 1),
+    (86, 'Hydro Pump', 29,0),
+    (87,'Future Sight', 29, 0),
+
+    (88, 'Roaring Moon', 30, 1),
+    (89, 'Iron Valiant', 30, 0),
+    (90, 'Flutter Mane', 30, 0)
+

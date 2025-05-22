79e767642348bb0bbb13c79fc43a734ee8df07e9
David Cano <davecano.244@gmail.com>
2025-05-22 10:53:05 -0400

    Positioned logout button in home.css

--- home.php
+++ home.php
@@ -30,14 +30,13 @@ try {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="home.css">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
 <body>
+<a class="btn btn-logout" role="button" href="logout.php">Log out</a>
 <div class="btn-toolbar" role="toolbar">
-    <div class="btn-group me-2" role="group" aria-label="First group">
-        <a class="btn btn-danger" role="button" href="logout.php">Log out</a>
-    </div>
     <div class="btn-group me-2" role="group" aria-label="Second group">
         <?php foreach($quizzes as $quiz): ?>
             <form method="post" action="quiz.php">
@@ -45,6 +44,8 @@ try {
                 <button type="submit" class="btn btn-info"><?= $quiz['name'] ?></button>
             </form>
         <?php endforeach; ?>
+        <div class="btn-group me-2" role="group" aria-label="First group">
+        </div>
     </div>
 </div>
 </body>
